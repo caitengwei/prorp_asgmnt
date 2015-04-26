c35dbfd2ba1e2dd5cf1ec89a6d398ed15c02bf8c
caitengwei <tengweicai@gmail.com>
2015-04-27 00:00:08 +0800

    Add Polynomial and delete useless file

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,7 +3,7 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b() * b() - a() * c() * 4)
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
