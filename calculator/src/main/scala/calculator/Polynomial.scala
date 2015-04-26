cc66bdd76433612bf2b87a487833edffdf5b6227
caitengwei <tengweicai@gmail.com>
2015-04-27 00:09:37 +0800

    Finished Polynomial part

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import scala.math.sqrt
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
@@ -8,6 +10,15 @@ object Polynomial {
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    def mySqrt(x: Signal[Double]): Signal[Double] = {
+      if (x() < 0) Signal(0)
+      else Signal(sqrt(x()))
+    }
+    Signal(
+      Set(
+        (-b() + mySqrt(computeDelta(a, b, c))()) / (2 * a()),
+        (-b() - mySqrt(computeDelta(a, b, c))()) / (2 * a())
+      )
+    )
   }
 }
